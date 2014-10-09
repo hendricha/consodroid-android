19b529a759ec115665bab49467a7f1b94a749c06
hendricha <hendricha@gmail.com>
2014-10-09 20:41:50 +0200

    create_obb.sh: Added building node.js and contextify for android

--- create_obb.sh
+++ create_obb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-NODESOURCE="http://nodejs.org/dist/v0.11.14/node-v0.11.14.tar.gz"
+#sudo apt-get install libbz2-dev
+NODEVERSION="v0.11.14"
+NODESOURCE="http://nodejs.org/dist/$NODEVERSION/node-$NODEVERSION.tar.gz"
+NDKVERSION="r10b"
+NDKSOURCE="http://dl.google.com/android/ndk/android-ndk32-$NDKVERSION-linux-x86_64.tar.bz2"
 JOBB="jobb"
 TEMPORARY="/tmp"
 CONSODROIDANDROIDDIR=$PWD
@@ -12,16 +16,45 @@ if [[ "$1" == "--help" ]]; then
   exit
 fi
 
+NDKDIR=$1
 if [[ "$1" == "--mock" ]]; then
   CONSODROIDREPOSITORY=$CONSODROIDMOCKREPOSITORY
+  NDKDIR=$2
 fi
 
 cd $TEMPORARY
+
 git clone $CONSODROIDREPOSITORY consoloid-consodroid
 cd consoloid-consodroid
 ./make_it_deployable.sh
+rm -rf node_modules/fs-ext
+rm -rf node_modules/contextify
+cd node_modules
+git clone https://github.com/hendricha/contextify.git
+rm -rf contextify/.git
 
-cp /tmp/node .
+cd $TEMPORARY
+
+if [[ "$1" == "--download-ndk" ]] || [[ "$2" == "--download-ndk" ]]; then
+  wget --output-document ndk.tar.bz2 $NDKSOURCE
+  tar jxvf ndk.tar.bz2
+  NDKDIR=$TEMPORARY/android-ndk-$NDKVERSION
+fi
+
+wget --output-document node.tar.gz $NODESOURCE
+tar xvf node.tar.gz
+cd node-$NODEVERSION
+
+sed 's/4.7/4.8/g' android-configure > android-configure4.8
+chmod +x android-configure4.8
+. ./android-configure4.8 $NDKDIR
+make
+
+cd $TEMPORARY/consoloid-consodroid/node_modules/contextify
+npm install binding nan
+cd build
+make
+cp $TEMPORARY/node-$NODEVERSION/node $TEMPORARY/consoloid-consodroid
 
 $JOBB -d . -o $CONSODROIDANDROIDDIR/app/src/main/assets/consodroid.obb -pn hu.hendricha.consodroid -pv 1
 echo $RANDOM > $CONSODROIDANDROIDDIR/app/src/main/assets/version
